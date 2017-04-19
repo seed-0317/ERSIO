4918d334aa04eb589a2621e221e0202006978755
Hamilton <hamilton.cory@gmail.com>
2017-04-19 12:47:42 -0400

    Added login.html and BusinessLogic class

---
+++ src/main/java/com.seed/Service/BusinessLogic.java
@@ -0,0 +1,39 @@
+package com.seed.Service;
+
+import com.seed.DAO.UserDao;
+import com.seed.DAO.UserDaoImpl;
+import com.seed.Model.User;
+import java.util.List;
+import java.util.Iterator;
+
+public class BusinessLogic {
+
+    private List<User> user;
+
+    public BusinessLogic() {
+    }
+
+    /**
+     * @param username
+     * @return person object if logged in successfully, otherwise {@code null}
+     */
+    public User login(String username) {
+        if (username == null || username.isEmpty()) {
+            // Could also: throw new IllegalArgumentException();
+            return null;
+        } else {
+            // Should call Dao
+            UserDao dao = new UserDaoImpl();
+            user = dao.retrieveUsers();
+
+            Iterator<User> iter = user.listIterator();
+            while (iter.hasNext()) {
+                User currentUser = iter.next();
+                if (currentUser.getUserID().equals(username)) {
+                    return currentUser;
+                }
+            }
+            return null;
+        }
+    }
+}
--- src/main/java/com.seed/Servlet/Login.java
+++ src/main/java/com.seed/Servlet/Login.java
@@ -1,4 +1,46 @@
 package com.seed.Servlet;
 
-public class Login {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.seed.Model.*;
+import com.seed.Service.BusinessLogic;
+
+@WebServlet(value="/login")
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("Login.html").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("userNameForm");
+
+        if(username == null) {
+            response.sendRedirect("login");
+        } else {
+            HttpSession session = request.getSession();
+
+            //Get user data and set attributes below:
+            BusinessLogic businessLogic = new BusinessLogic();
+
+            User user = businessLogic.login(username);
+            session.setAttribute("firstName", user.getFirstName());
+
+            //Send user to either employee home or manager home depending on UR_ROLE in ERSIO.ERS_USER_ROLES
+            if (session.getAttribute("UR_ROLE").equals("1")){
+                response.sendRedirect("EmployeeHome");
+            }
+            else {
+                response.sendRedirect("ManagerHome");
+            }
+        }
+    }
 }
