970510591ed1000875325f217d6b2b44b99304fa
Aleks Howell <aleksandra.howell@capitalone.com>
2017-04-22 09:31:46 -0400

    Added a servlet for create new expense

---
+++ src/main/java/com.seed/Servlet/CreateExpense.java
@@ -0,0 +1,52 @@
+package com.seed.Servlet;
+
+
+import com.seed.Model.Expense;
+import com.seed.Model.User;
+import com.seed.Service.BusinessLogic;
+import com.seed.Service.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(value="/createexpense")
+public class CreateExpense extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("CreateNewExpense.html").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session=request.getSession();
+        String idAuthor= (String) session.getAttribute("userID");
+
+        String amount=request.getParameter("SubmitAmount");
+        String descriptor=request.getParameter("SubmitDescriptor");
+        String type=request.getParameter("SubmitType");
+
+        BusinessLogic businessLogic=new BusinessLogic();
+        Expense newExpense =new Expense();
+        newExpense.setAmount(amount);
+        newExpense.setDescriptor(descriptor);
+        newExpense.setType(type);
+        newExpense.setIdAuthor(idAuthor);
+
+        businessLogic.createExpense(newExpense);
+
+
+                
+
+
+
+
+
+
+    }
+}
