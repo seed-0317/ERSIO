0b74f68a39ae7b1864ca7d99b00958edd95b4411
Benjamin Wilson <Benjamin.Wilson@CapitalOne.com>
2017-04-27 10:09:49 -0400

    fixed create exp&update profile

--- src/main/webapp/resources/scripts/custom.js
+++ src/main/webapp/resources/scripts/custom.js
@@ -6,24 +6,10 @@
 // On success, replace the HTML #main content with the html data in the HTTP response
 $(function(){
 
-    $('.hamburger').click(function (event) {
-            event.stopPropagation();
-            $('.overlay').toggle();
-        $('.hamburger').toggleClass('is-open is-closed');
-            $('#wrapper').toggleClass('toggled');
-        });
-
-
     var ajaxFunction = function(event){
         event.preventDefault();
         event.stopPropagation();
-        var target = $(event.target);
-        if(target.parents('nav').length > 0){
-            var trigger = $('.hamburger, nav .sidebar-nav li .servletLink, nav .sidebar-nav li a .servletLink');
-            $('.overlay').toggle();
-            trigger.toggleClass('is-open is-closed');
-            $('#wrapper').toggleClass('toggled');
-        };
+
         var ajaxMethod = $(this).data().method,
             ajaxURL = $(this).data().url,
             ajaxData = {};
@@ -32,14 +18,15 @@ $(function(){
              ajaxDataValue = obj.value;
             ajaxData[ ajaxDataKey ] = ajaxDataValue;
         });
-
         $.ajax({
             method   : ajaxMethod,
             url      : "/" + ajaxURL,
             data     : ajaxData,
             success  : function(html){
-                debugger;
                 $("#main").empty().append(html);
+                $(document).ready(function(){
+                    $('#buttonId').click(ajaxFunction);
+                });
                 $('.tablescroll').DataTable( {
                     scrollY:        '49vh',
                     scrollCollapse: true,
@@ -47,39 +34,20 @@ $(function(){
                 });
                 $('.' + ajaxURL).remove();
             },
-            statusCode: {
-                200: function(html){
-                    debugger;
-                    $("#main").empty().append(html);
-                    $('.tablescroll').DataTable( {
-                        scrollY:        '45vh',
-                        scrollCollapse: true,
-                        paging:         false
-                    });
-                    $('.' + ajaxURL).remove();
-                }
-            },
             error    : function(){console.log("error");},
-            complete    : function(){debugger;}
-
-
         });
-
     };
     $('.servletLink').click(ajaxFunction);
 });
 
+$(document).ready(function () {
+    var trigger = $('.hamburger, nav .sidebar-nav li .servletLink, nav .sidebar-nav li a .servletLink');
 
-//
-// $(document).ready(function () {
-//     var trigger = $('.hamburger, nav .sidebar-nav li .servletLink, nav .sidebar-nav li a .servletLink');
-//
-//     trigger.click(function (event) {
-//         event.stopPropagation();
-//         debugger;
-//         $('.overlay').toggle();
-//         trigger.toggleClass('is-open is-closed');
-//         $('#wrapper').toggleClass('toggled');
-//     });
-// });
-//
+    trigger.click(function (event) {
+        event.stopPropagation();
+        $('.overlay').toggle();
+        trigger.toggleClass('is-open is-closed');
+        $('#wrapper').toggleClass('toggled');
+    });
+});
+
